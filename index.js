b2251f58cd928bfb0cca612df68561ba2a2ca1e4
fatuma65 <namalefatuma28@gmail.com>
2024-09-04 19:59:19 +0300

    add products page and save items to local storage

---
+++ index.js
@@ -0,0 +1,123 @@
+let cartPage = document.getElementById("cart");
+let anchor = document.createElement("a");
+// when cart is clicked, we are taken to the cart page
+cartPage.addEventListener("click", () => {
+  let location = window.location.assign("index.html");
+  anchor.setAttribute("href", location);
+});
+cartPage.appendChild(anchor);
+let mainDiv = document.getElementById("myDiv");
+
+// create the products data
+let dataDetails = [
+  {
+    id: 1,
+    image: "images/after.jpg",
+    price: 12000,
+    count: 1,
+    title: "After Movie",
+    subTotal: 12000,
+  },
+  {
+    id: 2,
+    image: "images/bad-boys.jpg",
+    price: 17000,
+    count: 1,
+    title: "Bad boys Movie",
+    subTotal: 17000,
+  },
+  {
+    id: 3,
+    image: "images/aqua-man.jpg",
+    price: 18000,
+    count: 1,
+    title: "Aqua Man Movie",
+    subTotal: 18000,
+  },
+  {
+    id: 4,
+    image: "images/before.jpg",
+    price: 15000,
+    count: 1,
+    title: "Me Before You Movie",
+    subTotal: 15000,
+  },
+  {
+    id: 5,
+    image: "images/black-swan.jpg",
+    price: 15000,
+    count: 1,
+    title: "Me Before You Movie",
+    subTotal: 15000,
+  },
+  {
+    id: 6,
+    image: "images/everest.jpg",
+    price: 15000,
+    count: 1,
+    title: "Me Before You Movie",
+    subTotal: 15000,
+  },
+  {
+    id: 7,
+    image: "images/escape.jpg",
+    price: 15000,
+    count: 1,
+    title: "Me Before You Movie",
+    subTotal: 15000,
+  },
+  {
+    id: 8,
+    image: "images/fair-play.jpg",
+    price: 15000,
+    count: 1,
+    title: "Me Before You Movie",
+    subTotal: 15000,
+  },
+];
+
+const hanldeAddToCart = (id, addButton) => {
+  addButton.addEventListener("click", () => {
+    let itemsInStorage = localStorage.getItem("movies");
+    // if there are items in local storage, set the addedItems to that parsed version of those items else we put it becomes an empty array.
+    let addedItems = itemsInStorage ? JSON.parse(itemsInStorage) : [];
+    for (let i = 0; i < dataDetails.length; i++) {
+        // we find an item with a specific id and add it to the array as well as locl storage.
+      if (dataDetails[i].id === id) {
+        alert('Item added to cart successfully')
+        addedItems.push(dataDetails[i]);
+      }
+    }
+    localStorage.setItem("movies", JSON.stringify(addedItems));
+  });
+};
+
+const showItems = () => {
+  let divForFourItems;
+  dataDetails.forEach((data, index) => {
+
+    // show only 4 items each in a row.
+    if (index % 4 === 0) {
+      divForFourItems = document.createElement("div");
+      divForFourItems.style.display = "flex";
+      divForFourItems.style.justifyContent = "center";
+      divForFourItems.style.marginBottom = "20px";
+      mainDiv.appendChild(divForFourItems);
+    }
+    const divElement = document.createElement("div");
+    let addButton = document.createElement("button");
+    addButton.textContent = "Add to cart";
+    let id = data.id;
+    hanldeAddToCart(id, addButton);
+    divElement.style.margin = "10px";
+    let image = document.createElement("img");
+    image.setAttribute("src", data.image);
+    let price = document.createElement("p");
+    price.textContent = data.price;
+    let title = document.createElement("p");
+    title.innerText = data.title;
+    divElement.append(image, title, price, addButton);
+    divForFourItems.appendChild(divElement);
+  });
+};
+showItems();
--- script.js
+++ script.js
@@ -1,54 +1,90 @@
 const tableElement = document.querySelector("table");
 const tableBody = document.querySelector("tbody");
 
-let dataDetails = [
-  {
-    image: "The Hobbit movie",
-    price: 12000,
-    increase: "+",
-    decrease: "-",
-    count: 1,
-    subTotal: 12000,
-  },
-  {
-    image: "Bad boys movie",
-    price: 17000,
-    increase: "+",
-    decrease: "-",
-    count: 1,
-    subTotal: 12000,
-  },
-  {
-    image: "Aqua Man movie",
-    price: 18000,
-    increase: "+",
-    decrease: "-",
-    count: 1,
-    subTotal: 12000,
-  },
-];
+let back = document.getElementById("back");
+back.addEventListener("click", () => {
+  window.location.href = "home.html";
+});
+back.style.padding = "10px";
+back.style.backgroundColor = "#cbb157";
+back.style.color = "white";
+back.style.margin = "5px";
+
+const addedItems = localStorage.getItem("movies");
+const parsedItems = JSON.parse(addedItems);
+
 const handleQuantity = (
   increaseButton,
   negativeButton,
   paragraphButton,
+  subTotalElement,
   data
 ) => {
   increaseButton.addEventListener("click", () => {
+    // increase the count when the button is clicked
     data.count++;
     paragraphButton.textContent = data.count;
+    handleSubTotal(subTotalElement, data);
   });
   negativeButton.addEventListener("click", () => {
-    data.count--;
-    paragraphButton.textContent = data.count;
+    if (data.count > 1) {
+      // decrease the count when the button is clicked
+      data.count--;
+      paragraphButton.textContent = data.count;
+      handleSubTotal(subTotalElement, data);
+    }
   });
 };
-function handleData() {
-  dataDetails.forEach((data) => {
-    console.log(data);
+const handleSubTotal = (subTotalElement, data) => {
+  // when i click on the increase button, the subtotal amount increases as well
+  let total = data.price * data.count;
+  subTotalElement.textContent = total;
+};
 
+const handleRemove = (removeButton, tableRow, id) => {
+  removeButton.addEventListener("click", () => {
+    // loop through the array from local storage to find the selected item
+    for (let i = 0; i < parsedItems.length; i++) {
+      if (parsedItems[i].id === id) {
+        // remove the item with the id
+        parsedItems.splice(i, 1);
+        console.log("Item has been removed");
+        tableRow.remove();
+        // update local storage with the new storage
+        localStorage.setItem("movies", JSON.stringify(parsedItems));
+      }
+    }
+  });
+};
+const handleTotalAmount = () => {
+  const total = parsedItems.reduce((sum, element) => sum + element.subTotal, 0);
+  let totalDiv = document.querySelector("tfoot");
+  let totalSubRow = document.createElement("tr");
+  let totalSubPara = document.createElement("td");
+  let totalSubAmount = document.createElement("td");
+
+  totalDiv.style.marginTop = "10px";
+  totalSubRow.style.display = "flex";
+  totalSubRow.style.justifyContent = "space-around";
+  totalSubRow.style.margin = "10px";
+
+  totalSubPara.innerText = "Total";
+  totalSubPara.style.fontWeight = "600";
+
+  totalSubAmount.innerText = total;
+  totalSubAmount.style.marginLeft = "40px";
+
+  totalSubRow.append(totalSubPara, totalSubAmount);
+  totalDiv.appendChild(totalSubRow);
+  tableElement.append(totalDiv);
+};
+handleTotalAmount();
+function handleData() {
+  parsedItems.forEach((data) => {
+    // create the table rows in the DOM
     let tableRow = document.createElement("tr");
     let tableImageDetails = document.createElement("td");
-    let images = document.createElement("td");
+    let images = document.createElement("img");
     let tablePriceDetails = document.createElement("td");
     let tableQuantityDetails = document.createElement("td");
     let quantityDiv = document.createElement("div");
@@ -57,6 +93,17 @@ function handleData() {
     let paragraphButton = document.createElement("p");
     let subTotalElement = document.createElement("td");
 
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.style.marginTop = "30px";
+    removeButton.style.marginRight = "10px";
+    removeButton.style.backgroundColor = "#e27b65";
+    removeButton.style.color = "white";
+    removeButton.style.border = "none";
+    removeButton.style.padding = "8px";
+    removeButton.style.fontWeight = "600";
+    let id = data.id;
+    handleRemove(removeButton, tableRow, id);
     quantityDiv.style.display = "flex";
     quantityDiv.style.alignItems = "center";
     quantityDiv.style.justifyContent = "center";
@@ -67,26 +114,44 @@ function handleData() {
     negativeButton.style.width = "20px";
     negativeButton.style.height = "20px";
 
-    handleQuantity(increaseButton, negativeButton, paragraphButton, data);
+    images.style.width = "80px";
+    images.style.height = "80px";
 
+    handleQuantity(
+      increaseButton,
+      negativeButton,
+      paragraphButton,
+      subTotalElement,
+      data
+    );
     quantityDiv.append(negativeButton, paragraphButton, increaseButton);
     tableQuantityDetails.appendChild(quantityDiv);
     tableRow.append(
       tableImageDetails,
       tablePriceDetails,
       tableQuantityDetails,
-      subTotalElement
+      subTotalElement,
+      removeButton
     );
     tableBody.appendChild(tableRow);
 
-    images.textContent = data.image;
-    tablePriceDetails.innerText = data.price;
-    negativeButton.innerText = data.decrease;
-    increaseButton.innerText = data.increase;
-    paragraphButton.innerText = data.count;
-    subTotalElement.innerText = data.subTotal;
+    images.setAttribute("src", data.image);
 
+    tablePriceDetails.innerText = data.price;
+    negativeButton.innerText = "-";
+    increaseButton.innerText = "+";
+    paragraphButton.innerText = 1;
+    subTotalElement.innerText = data.subTotal;
     tableImageDetails.appendChild(images);
   });
 }
 handleData();
+
+
+
+// getting the total amount
+// get the sub total of each item
+// - loop through the parsed array
+// - get the subTotal of each element
+// - add all the totals together
+// add all the subtotals to get one total
